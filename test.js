e51671b28367668edfafebc29c0fecbaa1c894dd
Selvam Thirumal <selvamthirumal22@gmail.com>
2026-09-17 15:52:57 +0530

    changes more

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const add = require('./index.js')
 
 const result = add (50,20)
 
-if (result === 30){
+if (result === 70){
     console.log('test passed')    
 }else{
     console.log('test failed')
